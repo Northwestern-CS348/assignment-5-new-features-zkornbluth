ca8eead152382a0c74fdece1f388a9526812a696
Zachary Kornbluth <zkornbluth2007@gmail.com>
2019-03-10 16:56:56 -0500

    passed tests

--- student_code.py
+++ student_code.py
@@ -36,10 +36,12 @@ def buildFeatureStatement(tree):
     # the subject. You call languageTools.extractRoot on the parse tree
 
  ##### Your code to extractRoot here
+    r = languageTools.extractRoot(tree)
 
     # To get the subject of a verb, we use languageTools.extractSubject on the ROOT
 
 ##### Your code to extractSubject here
+    s = languageTools.extractSubject(r)
 
     # Once we have the primary noun, we then want to resolve it, that is, figure out what it refers
     # to using core.resolveObjectFOPC. Resolve object will give us a list of names that are referred to by the
@@ -47,6 +49,7 @@ def buildFeatureStatement(tree):
     # other functions
 
 ##### Your code to resolveObjectFOPC here -- this will build some FOPC and Assert it
+    n_list = core.resolveObjectFOPC(s)
 
     # Then we need to figure out what is going to modify it. To do this, we go back to our verb and for
     # any modifiers associted with it. These will either be prepositional phrases or adjectives.
@@ -55,11 +58,14 @@ def buildFeatureStatement(tree):
     # the names and will build any FOPC associated with prepositional objects it finds and assert it.
 
 ##### Your code to resolveObjectFOPC here -- this takes the root, the primary (suibject) and your names
+    core.findAndAttachPrepObjectsFOPC(r, s, n_list)
 
     # Next we pull out any features associated with the object and attach them to the object. We can use
     # core.findAndAssertFeaturesFOPC to do this.  Like core.findAndAttachPrepObjectsFOPC, it takes the ROOT and
     # a list of names and builds the FOPC associated with any adjectives it finds
 
 ##### Your code to findAndAssertFeaturesFOPC here -- this takes the root and your names
+    core.findAndAssertFeaturesFOPC(r, n_list)
 
 ##### Your code to findAndAssertDefinitionsFOPC -- this takes the root and your names
+    core.findAndAssertDefinitionsFOPC(r, n_list)
